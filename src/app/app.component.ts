b04b4c39e80d7190c9be3d68fea0833f175a8ddc
Charles Steinhardt <css@cssmbp.attlocal.net>
2017-07-04 22:37:44 -0500

    add css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+
+  title = 'Use TypeScript not Javascript!';
+  public results: Array<string> = new Array<string>();
+
+  ngOnInit(): void {
+    var result: string;
+   
+    for (let num = 10; num < 101; num++) {
+  
+      result = '';
+      // see if number is divisible by 4 nd 6
+      var div_4 = (num % 4 == 0);
+      var div_6 = (num % 6 == 0);
+
+      // set result to  Dr.
+      if (div_4)
+        result = 'Dr. ';
+
+      // set result to Who or Dr Who
+      if (div_6)
+        result += 'Who';
+
+      // otherwise, it was not evenly divisible and  display the number only
+      if (result.length == 0)
+        result = num.toString();
+
+      this.results.push(result);
+    }
+  }
 }
